f44c97fb21693ce5af56c6a6b38f5b2cc99bbd2f
Sourav-student <so34ur56@gmail.com>
2025-07-11 23:31:57 +0530

    Fix event mapping logic and improve no events found message in AdminEvent and Events components

--- Client/src/admin/pages/events/AdminEvent.jsx
+++ Client/src/admin/pages/events/AdminEvent.jsx
@@ -75,7 +75,7 @@ const AdminEvent = () => {
             <Loader /> :
             <div className="allthrebox Events-cards flex flex-wrap justify-center items-center gap-8 px-3 mb-5 font-sans" data-aos="fade-up">
               {
-               events.length > 0 && events.map((event, index) => event?.insta_url ? (
+                events.length > 0 && events.map((event, index) => (
                   <div className="bg-[#d03c19] w-auto rounded-lg h-[800px]" key={index}>
                     <InstagramEmbed
                       url={event.insta_url}
@@ -116,13 +116,13 @@ const AdminEvent = () => {
                       <button className='bg-[#bb1d1d] hover:bg-[#e01414] p-2 rounded-lg font-medium' onClick={() => handleDelete(index)}>Delete</button>
                     </div>
                   </div>
-                ) : null
+                )
                 )}
-                {
-                  events.length === 0 && !loading &&
-                  <div className="text-white text-2xl font-bold">No Events Found</div>
-                }
             </div>
+        }
+        {
+          events.length === 0 && !loading &&
+          <div className="text-white text-2xl font-bold">No Events Found</div>
         }
       </section>
     </div>
